38de39bada732e5bd305e3f525a8061ba0160bca
Arsen <info@uprithm.com>
2021-03-16 22:19:09 +0900

    changed mediadevices

--- script.js
+++ script.js
@@ -7,11 +7,41 @@ Promise.all([
 ]).then(startVideo)
 
 function startVideo() {
-  navigator.mediaDevices.getUserMedia(
-    { video: {} },
-    stream => video.srcObject = stream,
-    err => console.error(err)
-  )
+  // navigator.mediaDevices.getUserMedia(
+  //   { video: {} },
+  //   stream => video.srcObject = stream,
+  //   err => console.error(err)
+  // )
+  navigator.mediaDevices = navigator.mediaDevices || ((navigator.mozGetUserMedia || navigator.webkitGetUserMedia) ? {
+    getUserMedia: function(c) {
+      return new Promise(function(y, n) {
+        (navigator.mozGetUserMedia ||
+         navigator.webkitGetUserMedia).call(navigator, c, y, n);
+      });
+    }
+  } : null);
+  
+  if (!navigator.mediaDevices) {
+    console.log("getUserMedia() not supported.");
+    return;
+  }
+  
+  // Prefer camera resolution nearest to 1280x720.
+  
+  var constraints = { audio: true, video: { width: 1280, height: 720 } };
+  
+  navigator.mediaDevices.getUserMedia(constraints)
+  .then(function(stream) {
+    var video = document.querySelector('video');
+    video.srcObject = stream
+    video.onloadedmetadata = function(e) {
+      video.play();
+    };
+  })
+  .catch(function(err) {
+    console.log(err.name + ": " + err.message);
+  });
+  
 }
 
 video.addEventListener('play', () => {
