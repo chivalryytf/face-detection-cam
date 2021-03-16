d2c2e43989c6e7e79d2c5b02f900229a61a9d348
Arsen <info@uprithm.com>
2021-03-16 22:21:23 +0900

    changed mediadevices

--- script.js
+++ script.js
@@ -28,11 +28,10 @@ function startVideo() {
   
   // Prefer camera resolution nearest to 1280x720.
   
-  var constraints = { audio: true, video: { width: 1280, height: 720 } };
+  var constraints = { audio: false, video: { width: 1280, height: 720 } };
   
   navigator.mediaDevices.getUserMedia(constraints)
   .then(function(stream) {
-    var video = document.querySelector('video');
     video.srcObject = stream
     video.onloadedmetadata = function(e) {
       video.play();
